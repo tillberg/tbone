101f361d2a61ae8ae28aa1675f8e7fcc1be58e26
Dan Tillberg <dan@tillberg.us>
2014-07-17 00:37:43 +0000

    Be stricter about references to variables on the root object

--- src/export.js
+++ src/export.js
@@ -26,5 +26,5 @@ if (TBONE_DEBUG) {
 // This is used by BBVis to hook into the base model/collection/view
 // before they are modified.  You can, too.
 try{
-    dispatchEvent(new CustomEvent('tbone_loaded'));
+    root['dispatchEvent'](new root['CustomEvent']('tbone_loaded'));
 } catch(e) {}
