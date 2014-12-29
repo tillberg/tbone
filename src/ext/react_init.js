91dd82795a52d7c1038fda7f65f53ae63942e86b
Dan Tillberg <dan.tillberg@threatstack.com>
2014-12-29 16:50:18 -0500

    Update the React plugin w/ more work correct

--- src/ext/react_init.js
+++ src/ext/react_init.js
@@ -1,65 +1,84 @@
 var React = window['React'];
 if (React) {
-	var origCreateClass = React['createClass'];
-	React['createClass'] = function (origOpts) {
-		function cleanUpTScopes (inst) {
-			_.each(inst.tscopes || [], function (tscope) {
-				tscope.destroy();
-			});
-			inst.tscopes = null;
-		}
-		function doUpdate (inst) {
-			if (!inst.hasUpdateQueued) {
-				inst.hasUpdateQueued = true;
-				cleanUpTScopes(inst);
-				inst.forceUpdate();
-			}
-		}
-		function getWrapperFn (origFn, isFirstWrappedEvent) {
-			if (origFn || isFirstWrappedEvent) {
-				return function () {
-					var self = this;
-					if (isFirstWrappedEvent) {
-						cleanUpTScopes(self);
-						self.hasUpdateQueued = false;
-					}
-					var rval;
-					if (origFn) {
-						var firstRun = true;
-						var tscope = T(function () {
-							if (firstRun) {
-								rval = origFn.call(self);
-								firstRun = false;
-							} else {
-								doUpdate(self);
-							}
-						}, tbone.priority.view);
-						tscope.isView = true;
-						if (!self.tscopes) {
-							self.tscopes = [];
-						}
-						self.tscopes.push(tscope);
-					}
-					return rval;
-				};
-			} else {
-				return undefined;
-			}
-		}
+    var origCreateClass = React['createClass'];
+    React['createClass'] = function (origOpts) {
+        function cleanUpTScopes (inst) {
+            _.each(inst.tscopes || [], function (tscope) {
+                tscope.destroy();
+            });
+            inst.tscopes = null;
+        }
+        function doUpdate (inst) {
+            if (!inst.hasUpdateQueued) {
+                inst.hasUpdateQueued = true;
+                cleanUpTScopes(inst);
+                if (inst.isMounted()) {
+                    inst.forceUpdate();
+                    // console.log(inst.getDOMNode());
+                }
+            }
+        }
+        function getWrapperFn (origFn, isFirstWrappedEvent) {
+            if (origFn || isFirstWrappedEvent) {
+                return function () {
+                    var self = this, args = arguments;
+                    if (isFirstWrappedEvent) {
+                        cleanUpTScopes(self);
+                        self.hasUpdateQueued = false;
+                    }
+                    var rval;
+                    if (origFn) {
+                        var firstRun = true;
+                        var tscope = T(function () {
+                            if (firstRun) {
+                                rval = origFn.apply(self, args);
+                                // console.log('render', self._currentElement.type.displayName);
+                                firstRun = false;
+                            } else {
+                                // console.log('update', self._currentElement.type.displayName);
+                                doUpdate(self);
+                            }
+                        }, tbone.priority.view);
+                        tscope.isView = true;
+                        if (!self.tscopes) {
+                            self.tscopes = [];
+                        }
+                        self.tscopes.push(tscope);
+                    }
+                    return rval;
+                };
+            } else {
+                return undefined;
+            }
+        }
 
-		var opts = _.extend({}, origOpts, {
-			componentWillUnmount: function () {
-				cleanUpTScopes(this);
-				if (origOpts.componentWillUnmount) {
-					return origOpts.componentWillUnmount.apply(this, arguments);
-				} else {
-					return undefined;
-				}
-			},
-			componentWillUpdate: getWrapperFn(origOpts.componentWillUpdate, true),
-			componentDidUpdate: getWrapperFn(origOpts.componentDidUpdate, false),
-			render: getWrapperFn(origOpts.render, false)
-		});
-		return origCreateClass(opts);
-	};
+        var componentDidMount = origOpts.componentDidMount ? function () {
+            var self = this, args = arguments;
+            var rval;
+            T(function () {
+                // Run and re-run componentDidMount until this component is
+                // no longer mounted.
+                if (self.isMounted()) {
+                    rval = origOpts.componentDidMount.apply(self, args);
+                }
+            });
+            return rval;
+        } : undefined;
+
+        var opts = _.extend({}, origOpts, {
+            componentWillUnmount: function () {
+                cleanUpTScopes(this);
+                if (origOpts.componentWillUnmount) {
+                    return origOpts.componentWillUnmount.apply(this, arguments);
+                } else {
+                    return undefined;
+                }
+            },
+            componentDidMount: componentDidMount,
+            componentWillUpdate: getWrapperFn(origOpts.componentWillUpdate, true),
+            componentDidUpdate: getWrapperFn(origOpts.componentDidUpdate, false),
+            render: getWrapperFn(origOpts.render, false)
+        });
+        return origCreateClass(opts);
+    };
 }
